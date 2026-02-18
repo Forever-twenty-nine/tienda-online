3806868019cb2d5eeb542dfbce347bdcdbfbadb4
Natalia <alamo.natalia@gmail.com>
2026-02-18 21:43:57 +0100

    feat: quitando clase container y acomodando estilos de páginas estáticas

--- src/app/modules/public/home/home.ts
+++ src/app/modules/public/home/home.ts
@@ -7,27 +7,32 @@ import { ProductCard } from '../../components/product-card/product-card';
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink, ProductCard],
-  templateUrl: './home.html'
+  templateUrl: './home.html',
 })
 export class Home {
   productsService = inject(ProductsService);
 
-  defaultHeroImage = 'https://images.unsplash.com/photo-1549439602-43ebca2327af?q=80&w=2070&auto=format&fit=crop';
+  defaultHeroImage =
+    'https://firebasestorage.googleapis.com/v0/b/tienda-online-50dda.firebasestorage.app/o/products%2F1770994343043_pulsera-2.jpg?alt=media&token=c6885275-a59c-4ad4-a8bb-49e4ad2a9b11';
 
   heroProduct = computed(() => {
     const products = this.productsService.products();
     if (products.length === 0) return null;
 
     // Productos con descuento
-    const discounted = products.filter(p => (p.descuento ?? 0) > 0);
-    const pool = discounted.length > 0 ? discounted : products.filter(p => p.destacado);
-    
+    const discounted = products.filter((p) => (p.descuento ?? 0) > 0);
+    const pool =
+      discounted.length > 0 ? discounted : products.filter((p) => p.destacado);
+
     if (pool.length === 0) return products[0];
 
     return pool[Math.floor(Math.random() * pool.length)];
   });
 
   get featuredProducts() {
-    return this.productsService.products().filter(p => p.destacado).slice(0, 4);
+    return this.productsService
+      .products()
+      .filter((p) => p.destacado)
+      .slice(0, 4);
   }
 }
